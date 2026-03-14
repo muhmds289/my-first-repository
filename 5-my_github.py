2ec875fb60a5a523f08ca28f7217d39c4199d847
Mohamed Ibrahim <mohamedibrahim@Mohameds-MacBook-Pro.fritz.box>
2026-03-14 03:23:17 +0100

    Task 5: GitHub ID via Basic Auth (requests + sys)

--- 5-my_github.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import requests
-import sys
-
-username = sys.argv[1]
-password = sys.argv[2]
-
-response = requests.get('https://api.github.com/user', auth=(username, password))
-print(response.json().get('id'))
